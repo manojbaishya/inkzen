4c0265aff9f43d33fa949eab52a44dbd470b3453
Manoj Baishya <5944629-manojbaishya@users.noreply.gitlab.com>
2024-02-29 12:38:05 +0530

    Initialized project

---
+++ Inkzen.Api/Program.cs
@@ -0,0 +1,105 @@
+using Microsoft.EntityFrameworkCore;
+using Piranha;
+using Piranha.AttributeBuilder;
+using Piranha.AspNetCore.Identity.SQLite;
+using Piranha.Data.EF.SQLite;
+using Piranha.Manager.Editor;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace Inkzen.Api;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Development");
+
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+        builder.AddPiranha(options =>
+        {
+
+            options.UseCms();
+            options.UseManager();
+
+            options.UseFileStorage(naming: Piranha.Local.FileStorageNaming.UniqueFolderNames);
+            options.UseImageSharp();
+            options.UseTinyMCE();
+            options.UseMemoryCache();
+
+            options.UseApi(api => api.AllowAnonymousAccess = true);
+
+            string connectionString = builder.Configuration.GetConnectionString("piranha");
+            options.UseEF<SQLiteDb>(db => db.UseSqlite(connectionString));
+            options.UseIdentityWithSeed<IdentitySQLiteDb>(db => db.UseSqlite(connectionString));
+
+            /**
+             * Here you can configure the different permissions
+             * that you want to use for securing content in the
+             * application.
+
+            options.UseSecurity(o =>
+            {
+                o.UsePermission("WebUser", "Web User");
+            });
+
+             */
+
+            /**
+             * Here you can specify the login url for the front end
+             * application. This does not affect the login url of
+             * the manager interface.
+
+            options.LoginUrl = "login";
+
+             */
+        });
+
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo { Title = "PiranhaCMS API", Version = "v1" });
+            options.CustomSchemaIds(x => x.FullName);
+        });
+
+
+        WebApplication app = builder.Build();
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(opt =>
+            {
+                opt.SwaggerEndpoint("/swagger/v1/swagger.json", "PiranhaCMS API V1");
+            });
+        }
+
+
+        app.UsePiranha(options =>
+        {
+            // Initialize Piranha
+            App.Init(options.Api);
+
+            options.UseManager();
+
+            // Build content types
+            new ContentTypeBuilder(options.Api)
+                .AddAssembly(typeof(Program).Assembly)
+                .Build()
+                .DeleteOrphans();
+
+            // Configure Tiny MCE
+            EditorConfig.FromFile("editorconfig.json");
+            options.UseTinyMCE();
+
+            options.UseIdentity();
+        });
+
+        app.Run();
+    }
+}
